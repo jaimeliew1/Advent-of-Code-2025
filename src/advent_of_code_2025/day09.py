17c9a2c675db06d7d2cbda6e4aafbad0abea7c6f
Jaime Liew <jaimeliew1@gmail.com>
2025-12-10 19:02:56 +0100

    add day09

--- src/advent_of_code_2025/day09.py
+++ src/advent_of_code_2025/day09.py
@@ -1,17 +1,31 @@
+from itertools import combinations, pairwise
+
+
 def parse(fn: str) -> list[tuple[float]]:
     out = []
     for line in open(fn).readlines():
         a, b = line.strip().split(",")
         out.append((int(a), int(b)))
+    out.append(out[0])
     return out
 
 
-def solve(coords: list[tuple[float]]) -> tuple[float, float]:
-    ans1 = 0
-    for i, (a, b) in enumerate(coords):
-        for aa, bb in coords[i + 1 :]:
-            ans1 = max(ans1, abs((a - aa + 1) * (b - bb + 1)))
-    return ans1, 0
+def area(a, b, aa, bb) -> int:
+    return (aa - a + 1) * (bb - b + 1)
+
+
+def solve(polygon: list[tuple[float]]) -> tuple[float, float]:
+    pairs = []
+    for (a, b), (aa, bb) in combinations(polygon, r=2):
+        pairs.append((min(a, aa), min(b, bb), max(a, aa), max(b, bb)))
+    pairs = sorted(pairs, key=lambda x: area(*x), reverse=True)
+
+    for a, b, aa, bb in pairs:
+        for (p, q), (r, s) in pairwise(polygon):
+            if p < aa and r > a and q < bb and s > b:
+                break
+        else:
+            return area(*pairs[0]), area(a, b, aa, bb)
 
 
 if __name__ == "__main__":
