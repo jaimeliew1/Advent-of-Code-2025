6cfef02cd840da01a6965e901ab51e9627060bfb
Jaime Liew <jaimeliew1@gmail.com>
2025-12-10 20:06:30 +0100

    simplify day08

--- src/advent_of_code_2025/day08.py
+++ src/advent_of_code_2025/day08.py
@@ -1,46 +1,32 @@
-import math
+from itertools import combinations
 
 
 def parse(fn: str) -> list[tuple[float]]:
-    out = []
-    for line in open(fn).readlines():
-        a, b, c = line.split(",")
-        out.append((int(a), int(b), int(c)))
-    return out
+    return [eval(p) for p in open(fn).readlines()]
 
 
 def solve(coords) -> tuple[float, float]:
-    pairs = []
-    for i, (a, b, c) in enumerate(coords):
-        for j, (aa, bb, cc) in enumerate(coords[i + 1 :]):
-            pairs.append((i, i + j + 1, (a - aa) ** 2 + (b - bb) ** 2 + (c - cc) ** 2, a * aa))
-    pairs.sort(key=lambda x: x[2])
-
+    pairs = sorted(combinations(coords, 2), key=lambda x: sum((a - b) ** 2 for a, b in zip(*x)))
     sets = []
-    for i, (a, b, _, xx) in enumerate(pairs):
-        set_a = next((s for s in sets if a in s), None)
-        set_b = next((s for s in sets if b in s), None)
+    for i, (p1, p2) in enumerate(pairs):
+        set_a = next((s for s in sets if p1 in s), None)
+        set_b = next((s for s in sets if p2 in s), None)
         match set_a, set_b:
             case None, None:
-                sets.append({a, b})
+                sets.append({p1, p2})
             case _set_a, None:
-                _set_a.add(b)
+                _set_a.add(p2)
             case None, _set_b:
-                _set_b.add(a)
-            case _set_a, _set_b if _set_a == _set_b:
-                pass
-            case _set_a, _set_b:
+                _set_b.add(p1)
+            case _set_a, _set_b if _set_a != _set_b:
                 set_a |= set_b
                 sets.remove(set_b)
 
         if i == 999:
-            lengths_sorted = sorted((len(x) for x in sets), reverse=True)
-            ans1 = math.prod(lengths_sorted[:3])
+            lens = sorted((len(x) for x in sets), reverse=True)
+            ans1 = lens[0] * lens[1] * lens[2]
         if len(sets[0]) == len(coords):
-            ans2 = xx
-            break
-
-    return ans1, ans2
+            return ans1, p1[0] * p2[0]
 
 
 if __name__ == "__main__":
